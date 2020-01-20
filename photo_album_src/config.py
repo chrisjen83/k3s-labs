5efeec97c23ceeb7dc60fee5905031893676cb2c
chrisjen83 <chrisjen83@gmail.com>
2020-01-20 19:50:17 +1100

    remove files

--- photo_album_src/config.py
+++
@@ -1,6 +0,0 @@
-ecs_test_drive = {
-    'ecs_endpoint_url' : 'https://object.ecstestdrive.com',
-    'ecs_access_key_id' : '131701420476682255@ecstestdrive.emc.com',
-    'ecs_secret_key' : 'dDp8MeNm1M5NiwkSs3gN+OhUu16VsetoWKOEQywi',
-    'ecs_bucket_name' : 'photo-album'
-}
